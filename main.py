9b3376af6621adc21ba046905d72f82aa22ce48b
btsmith29 <benjamin.t.smith@gmail.com>
2024-03-15 22:31:02 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+main assignment script
+
+Usage:
+  main [--download]
+
+Options:
+  --download  download data from source
+"""
+
+from docopt import docopt
+
+
+def main(download=False):
+
+
+if __name__ == "__main__":
+    cmd_args = core.handle_docopt_arguments(docopt(__doc__))
+    main(**cmd_args)
