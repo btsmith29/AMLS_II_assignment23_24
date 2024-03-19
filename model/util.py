346a5f08575f62dcda6e782815a5cda1f91fea25
btsmith29 <benjamin.t.smith@gmail.com>
2024-03-19 08:50:03 +0000

    first cut of model utils

---
+++ model/util.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+import keras
+import pandas as pd
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers, callbacks
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+
+def use_pre_trained_model_convnext_tiny() -> Model:
+    
+    base_model = tf.keras.applications.ConvNeXtTiny(weights='imagenet', include_top=False)
+    base_model.trainable = False
+    
+    return keras.Sequential([
+        
+        tf.keras.Input(shape=(IMAGE_SIZE, IMAGE_SIZE, 3)),
+        
+        base_model,
+        layers.GlobalAveragePooling2D(),
+
+        # Classifier Head
+        layers.Flatten(),
+        layers.Dense(1024, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(1024, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(units=5, activation="softmax"),
+    ])
+
+
+def run_experiment_lr(exp_id: str, sub_exp_id: int, model_fn: str, ds_train_, ds_valid_):
+    
+    #data = {"exp_id":"1", "desc":"something", "batch_size":BATCH_SIZE, "img_width":0, "img_height":0, "model": model_str}
+    #df_experiment = pd.concat([pd.DataFrame([data]), df_experiment])
+    
+    model = model_fn()
+    
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(epsilon=0.005),
+        loss="categorical_crossentropy",
+        metrics=['accuracy']
+    )
+
+    early_stopping = callbacks.EarlyStopping(
+        min_delta=0.0001,
+        patience=ES_PATIENCE,
+        restore_best_weights=True,
+    )
+    
+    reduce_lr = callbacks.ReduceLROnPlateau(monitor = 'val_loss', factor = 0.3, 
+                                            patience = 3, min_delta = 0.001, 
+                                            mode = 'min', verbose = 1)
+    
+    cbs = []
+    if ES:
+        print("Using EarlyStopping")
+        cbs += [early_stopping]
+    if LR:
+        print("Using ReduceLROnPlateau")
+        cbs += [reduce_lr]
+
+    history = model.fit(
+        ds_train_,
+        validation_data=ds_valid_,
+        epochs=EPOCHS,
+        verbose=1,
+        callbacks=cbs
+    )
+    
+    df_hist = pd.DataFrame(history.history)
+    df_hist = df_hist.reset_index()
+    df_hist["exp_id"] = exp_id
+    df_hist["sub_exp_id"] = sub_exp_id
+    df_hist["epoch"] = df_hist.index
+   
+    return model, df_hist
+
+
+def add_results(df_all_results, df_new_results):
+    if df_all_results.empty:
+        return df_new_results
+    else:
+        return pd.concat([df_all_results, df_new_results])
