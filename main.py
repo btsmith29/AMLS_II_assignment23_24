b47ddf4ce0b7b1f586733d921af76b4fad47f976
btsmith29 <benjamin.t.smith@gmail.com>
2024-03-19 09:09:19 +0000

    fix model ref

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main(download=False):
   BATCH_SIZE = 196
 
   # ds_train, ds_valid = get_equal_split_aug_dataset_raw(dataset_path, 2)
-  (m, df_hist) = model_util.run_experiment_lr("use_pre_trained_model_convnext_tiny", 1, use_pre_trained_model_convnext_tiny, ds_train, ds_valid)
+  (m, df_hist) = model_util.run_experiment_lr("use_pre_trained_model_convnext_tiny", 1, model_util.use_pre_trained_model_convnext_tiny, ds_train, ds_valid)
   df_results = model_util.add_results(df_results, df_hist)
     
   df_results.to_csv("results.csv")
