15924a06015264639976ce304bc68b5655487901
btsmith29 <benjamin.t.smith@gmail.com>
2024-03-25 09:32:38 +0000

    use params

--- data_processing/pre_processing.py
+++ data_processing/pre_processing.py
@@ -11,56 +11,57 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras import layers, callbacks
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing import image_dataset_from_directory
+from typing import Tuple
 
 
-# ultimately, need to pass these as params
-IMAGE_SIZE = 255
-BATCH_SIZE = 196
-
-
-def download_data(path: Path) -> None:
-    url = "https://drive.google.com/uc?id=1TJBf1HZxAMpowZ92BcgS5N_NPHE7LPOT"
-    output = path / "data.zip"
-    gdown.download(url, str(output), quiet=False)
-    return output
-    
-
-def data_preprocessing(path: Path):
-    file = download_data(path) # pass data_path
+def data_preprocessing(path: Path, params: Params, force=False)
+    -> Tuple[Dataset, Dataset, Dataset, dict]:
+    """
+    """
+    file = download_data(path, force)
     
     data_path = path / "data"
-    data_path.mkdir(parents=True, exist_ok=True)
-    
-    with zipfile.ZipFile(file, "r") as z:
-        z.extractall(data_path)
+    if force:
+        shutil.rmtree(data_path)
+        
+    if not data_path.exists():
+        data_path.mkdir(parents=True, exist_ok=True)
+       
+        with zipfile.ZipFile(file, "r") as z:
+            z.extractall(data_path)
         
     df_images = pd.read_csv((data_path / "train.csv"))
+   
+    X_train, X_test, y_train, y_test
+        = train_test_split(df_images.image_id, df_images.label, test_size=0.2, random_state=12)
     
-    X_train, X_test, y_train, y_test = train_test_split(df_images.image_id, df_images.label, test_size=0.2, random_state=12)
-    
-    X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.25, random_state=12)
+    X_train, X_valid, y_train, y_valid
+        = train_test_split(X_train, y_train, test_size=0.25, random_state=12)
     
     train_path = create_ds_tree(X_train, y_train, data_path, "train")
     valid_path = create_ds_tree(X_valid, y_valid, data_path, "valid")
     test_path = create_ds_tree(X_test, y_test, data_path, "test")
     
-    ds_train = create_dataset(train_path)
-    ds_valid = create_dataset(valid_path)
-    ds_test = create_dataset(test_path)
-    
-    # clean-up
-    os.rmdir((data_path / "train_images"))
-    os.remove(file)
-                            
-    return ds_train, ds_valid, ds_test
+    ds_train = create_dataset(train_path, params.image_size, params.batch_size)
+    ds_valid = create_dataset(valid_path, params.image_size, params.batch_size)
+    ds_test = create_dataset(test_path, params.image_size, params.batch_size)
+
+    return ds_train, ds_valid, ds_test, extract_class_weights(df_images)
 
 
-
-
-
-def create_ds_tree(x, y, path, name):
+def download_data(path: Path, force=False) -> Path:
     """
-    
+    """
+    url = "https://drive.google.com/uc?id=1TJBf1HZxAMpowZ92BcgS5N_NPHE7LPOT"
+    output = path / "data.zip"
+    if not Path(output).exists() or force:
+        gdown.download(url, str(output), quiet=False)
+    return output
+
+
+def create_ds_tree(x, y, path: Path, name: str) -> Path:
+    """
+    Creates the directory structure for the given dataset.
     """
     ds_path = path / name
     if not ds_path.exists():
@@ -79,11 +80,43 @@ def create_ds_tree(x, y, path, name):
     return ds_path
 
 
-def create_dataset(path: Path):
+def create_dataset(path: Path, img_size: int, batch_size: int = None) -> Dataset:
+    """
+    """
     return image_dataset_from_directory(
         path,
         labels='inferred',
         label_mode='categorical',
-        image_size=[IMAGE_SIZE, IMAGE_SIZE],
-        batch_size=BATCH_SIZE,
-    )   
+        image_size=[img_size, img_size],
+        batch_size=batch_size,
+        seed=12345,
+    )
+
+
+def extract_class_weights(df_data: pd.DataFrame) -> dict:
+    classes = df_data.label.unique()
+    class_weights = compute_class_weight(class_weight='balanced',
+                                         classes=classes,
+                                         y=df_data.label)
+
+    return dict(zip(classes, class_weights))
+
+
+def augment_dataset(ds: Dataset, num_repeats: int) -> Dataset:
+    """
+    """
+    def augment(image, label):
+        seed = 12345
+        image = tf.image.random_flip_left_right(image, seed)
+        image = tf.image.random_flip_up_down(image, seed)
+        image = tf.image.random_brightness(image, 0.2, seed)
+        return image, label
+
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
+    return (
+        ds
+        .repeat(num_repeats)
+        .map(augment)
+        .cache()
+        .prefetch(buffer_size=AUTOTUNE)
+    )
