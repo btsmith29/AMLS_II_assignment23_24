f959889dce2c0a0c46c7eeeae465293482da1b19
btsmith29 <benjamin.t.smith@gmail.com>
2024-03-25 22:18:46 +0000

    Add dividing sections

--- main.py
+++ main.py
@@ -24,35 +24,24 @@ def main(download=False):
   tf.random.set_seed(67890)
 
   #DEFAULT_PARAMS = model_util.Params(255, 196, 50, True, 5, False)
-  DEFAULT_PARAMS = model_util.Params(100, 196, 2, True, 5, False)
+  DEFAULT_PARAMS = model_util.Params(50, 196, 1, True, 5, False)
   print(DEFAULT_PARAMS)
 
   ARTIFACTS_PATH = Path("artefacts").mkdir(parents=True, exist_ok=True)
 
   # Process Data
+  print("==== Loading Data ====")
   cwd = os.getcwd()
   ds_train, ds_valid, ds_test, class_weights = data.data_preprocessing(Path(cwd), DEFAULT_PARAMS)
   print(f"Class Weights: {class_weights}")
 
+  print("==== Task A: Baseline Model ====")
   baseline_model = model_util.create_model(tf.keras.applications.ConvNeXtBase, "baseline_model", DEFAULT_PARAMS)
   df_train, df_test = model_util.run_task("convnextbase", baseline_model, ds_train, ds_valid, ds_test, DEFAULT_PARAMS)
-
   print(df_train)
-  print(ds_test)
-  
-  # df_results = pd.DataFrame
-  # ES = True
-  # LR = False
-  # IMAGE_SIZE = 255
-  # BATCH_SIZE = 196
+  print(df_test)
 
-  # # ds_train, ds_valid = get_equal_split_aug_dataset_raw(dataset_path, 2)
-  # (m, df_hist) = model_util.run_experiment_lr("use_pre_trained_model_convnext_tiny", 1, model_util.use_pre_trained_model_convnext_tiny, ds_train, ds_valid)
-  # df_results = model_util.add_results(df_results, df_hist)
-    
-  # df_results.to_csv("results.csv")
-
-  # m.evaluate(ds_test)
+  print("==== Task B: Baseline + Data Augmentation ====")
 
 
 if __name__ == "__main__":
